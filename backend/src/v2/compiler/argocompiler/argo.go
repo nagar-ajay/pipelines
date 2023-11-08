b556d725595d095763c62b2fd2535eadeae4c929
nagar-ajay <ajay.nagar@nutanix.com>
2023-11-08 18:52:27 +0530

    use tag instead of SHA digest

--- backend/src/v2/compiler/argocompiler/argo.go
+++ backend/src/v2/compiler/argocompiler/argo.go
@@ -117,7 +117,7 @@ func Compile(jobArg *pipelinespec.PipelineJob, kubernetesSpecArg *pipelinespec.S
 		templates: make(map[string]*wfapi.Template),
 		// TODO(chensun): release process and update the images.
 		driverImage:   "gcr.io/ml-pipeline/kfp-driver@sha256:8e60086b04d92b657898a310ca9757631d58547e76bbbb8bfc376d654bef1707",
-		launcherImage: "quay.io/ajaynagar/kubeflow-ntnx/kfp-launcher@sha256:5ce2158b5b642edf2bf6896a8a1aa9f4baaf6c5cb717f8d580c659f46c39e608",
+		launcherImage: "quay.io/ajaynagar/kubeflow-ntnx/kfp-launcher:2.0.3",
 		job:           job,
 		spec:          spec,
 		executors:     deploy.GetExecutors(),
